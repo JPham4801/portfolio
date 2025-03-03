30583eb19dcef0d8a2b178b91d5f74614ffcf0bb
JPham4801 <JPham4801@gmail.com>
2025-03-03 10:25:18 -0600

    working typewriter effect and added skill badges

--- js/app.js
+++ js/app.js
@@ -2,6 +2,53 @@ const carouselList = document.querySelector('.carousel__list');
 const carouselItems = document.querySelectorAll('.carousel__item');
 const elems = Array.from(carouselItems);
 
+// typewriter effect
+
+class TxtType {
+  constructor(el, toRotate, period) {
+    this.toRotate = toRotate;
+    this.el = el;
+    this.loopNum = 0;
+    this.period = parseInt(period, 10) || 2000;
+    this.txt = '';
+    this.isDeleting = false;
+    this.tick();
+  }
+
+  tick = () => {
+    const i = this.loopNum % this.toRotate.length;
+    const fullTxt = this.toRotate[i];
+
+    if (this.isDeleting) {
+      this.txt = fullTxt.substring(0, this.txt.length - 1);
+    } else {
+      this.txt = fullTxt.substring(0, this.txt.length + 1);
+    }
+
+    this.el.innerHTML = `<span class="wrap">${this.txt}</span>`;
+
+    let delta = 200 - Math.random() * 100;
+    if (this.isDeleting) {
+      delta /= 2;
+    }
+
+    if (!this.isDeleting && this.txt === fullTxt) {
+      delta = this.period;
+      this.isDeleting = true;
+    } else if (this.isDeleting && this.txt === '') {
+      this.isDeleting = false;
+      this.loopNum++;
+      delta = 500;
+    }
+
+    setTimeout(() => {
+      this.tick();
+    }, delta);
+  };
+}
+
+// carousel
+
 carouselList.addEventListener('click', function (event) {
   const newActive = event.target.closest('.carousel__item');
 
@@ -35,3 +82,14 @@ function getPos(current, active) {
   }
   return diff;
 }
+
+window.onload = () => {
+  const elements = document.getElementsByClassName('typewrite');
+  for (let i = 0; i < elements.length; i++) {
+    const toRotate = elements[i].getAttribute('data-type');
+    const period = elements[i].getAttribute('data-period');
+    if (toRotate) {
+      new TxtType(elements[i], JSON.parse(toRotate), period);
+    }
+  }
+};
